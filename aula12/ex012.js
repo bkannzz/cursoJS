c71f0e19e007e746f2bdb20aa4ef5b17606a9f1b
rebecca <123714003+rebeccalss@users.noreply.github.com>
2023-09-16 00:51:55 -0300

    atualizando

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -4,6 +4,6 @@ if (idade < 16){
     console.log('Menor de idade')
 } else if (idade < 18 || idade > 65){
     console.log('Voto opcional')
-} else if (idade >= 18){
+} else {
     console.log('Voto obrigatório')
 }
