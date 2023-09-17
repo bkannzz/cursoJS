89494c59e909b6022338d8da8193450aba67e587
rebecca <123714003+rebeccalss@users.noreply.github.com>
2023-09-17 05:49:48 -0300

    atualizando

--- aula13ex/ex014/script.js
+++ aula13ex/ex014/script.js
@@ -6,15 +6,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.` 
     if (hora >= 0 && hora < 12){
         // BOM DIA!
-        img.src = 'dia.jpg'
+        img.src = 'imagens/dia.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18){
         // BOA TARDE!
-        img.src = 'tarde.jpg'
+        img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
         // BOA NOITE!
-        img.src = 'noite.jpg'
+        img.src = 'imagens/noite.jpg'
         document.body.style.background = '#515154'
     }
 }
